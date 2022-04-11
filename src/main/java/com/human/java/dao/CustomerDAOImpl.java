ea6002a3dd09eabc850de67e9082d40e5328bfdc
xdo19 <52537296+xdo19@users.noreply.github.com>
2022-04-11 17:06:25 +0900

    minsung commit
    2022-04-11 / 17:07
    찜목록 insert

--- src/main/java/com/human/java/dao/CustomerDAOImpl.java
+++ src/main/java/com/human/java/dao/CustomerDAOImpl.java
@@ -1,10 +1,18 @@
 package com.human.java.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.human.java.domain.CustomerVO;
+import com.human.java.domain.ExhibitionVO;
+import com.human.java.domain.WishListVO;
 
 @Repository("customerDAO")
 public class CustomerDAOImpl implements  CustomerDAO{
@@ -41,6 +49,26 @@ public class CustomerDAOImpl implements  CustomerDAO{
 		
 		mybatis.update("customerMapper.customerchangeInfo",vo);
 	}
+
+	@Override
+	public void wishListInsert(ExhibitionVO vo, String id, HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		System.out.println("in");
+		Map<String, Object> parms = new HashMap<String, Object>();
+		parms.put("vo", vo);
+		parms.put("id", id);
+		String userId = (String)request.getSession().getAttribute("userId");
+		parms.put("request", userId);
+		mybatis.insert("wishlistMapper.wishlistInsert", parms);
+	}
+
+	@Override
+	public List<WishListVO> wishListSearch(WishListVO wl, HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 	
 	
 }
--- src/main/java/com/human/java/service/CustomerService.java
+++ src/main/java/com/human/java/service/CustomerService.java
@@ -1,6 +1,12 @@
 package com.human.java.service;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.human.java.domain.CustomerVO;
+import com.human.java.domain.ExhibitionVO;
+import com.human.java.domain.WishListVO;
 
 
 public interface CustomerService {
@@ -13,5 +19,8 @@ public interface CustomerService {
 	
 	//정보수정
 	public void customerupdate(CustomerVO vo);
+	
+	//찜목록
+	public List<WishListVO> WishListService(ExhibitionVO vo, String id, WishListVO wl,  HttpServletRequest request);
 
 }
